e955c4c457b260c2f4c80a165e74a9286124385e
Ola Adedoyin <olalinux03@gmail.com>
2020-05-30 01:29:54 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Boilerplate with Parcel</h1>
+      <p>A quick starter for react project. Build process using Parcel</p>
+    </div>
+  );
+};
+
+export default App;
